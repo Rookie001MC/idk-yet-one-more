0a3ad539106a95486500d6999d2184986e110d41
Rookie Nguyen <nhqthang@proton.me>
2026-04-26 21:24:28 +0700

    chore: initial site config

---
+++ src/lib/config.ts
@@ -0,0 +1,23 @@
+import { SiFacebook, SiInstagram, SiBluesky, SiGithub } from '@icons-pack/svelte-simple-icons';
+import { Mail } from '@lucide/svelte';
+import { PUBLIC_BASE_URL } from '$env/static/public';
+
+const siteConfig = {
+	name: "Rookie's Blog",
+	description: 'A blog about my life, my thoughts, my experiences, and many more.',
+	baseUrl: PUBLIC_BASE_URL || 'https://itsrookienguyen.me',
+	social: [
+		{ name: 'Facebook', url: 'https://facebook.com/realrookie001', icon: SiFacebook },
+		{ name: 'Instagram', url: 'https://instagram.com/realrookie001', icon: SiInstagram },
+		{ name: 'Bluesky', url: 'https://bsky.app/profile/itsrookienguyen.me', icon: SiBluesky },
+		{ name: 'Email', url: 'mailto:nhqthang@itsrookienguyen.me', icon: Mail },
+		{ name: 'GitHub', url: 'https://github.com/rookie001mc', icon: SiGithub }
+	],
+	staticPages: [
+		{ label: 'Home', path: '/' },
+		{ label: 'About', path: '/about' },
+		{ label: 'Blog', path: '/blog' }
+	]
+};
+
+export default siteConfig;
