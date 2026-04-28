9f0bd3ab201ccb7301021c1b1cbd6edcc032741d
Rookie Nguyen <nhqthang@proton.me>
2026-04-28 17:31:16 +0700

    where the fuck did you come from

--- src/lib/types/schema.ts
+++ src/lib/types/schema.ts
@@ -2,13 +2,11 @@ import type BlogPost from './blogPost';
 import type Category from './category';
 import type Tag from './tag';
 import type Page from './page';
-import type AiPrompt from './aiPrompt';
 
 export interface Schema {
 	blogPosts: BlogPost[];
 	categories: Category[];
 	tags: Tag[];
 	pages: Page[];
-	ai_prompts: AiPrompt[];
 	blogPostsTags: { id: number; blogPostsId: number; tagsId: number }[];
 }
