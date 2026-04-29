6f4c3e4e6a006453924f221174bb263f788e1e8a
Rookie Nguyen <nhqthang@proton.me>
2026-04-29 15:15:39 +0700

    fix(urgent): turning on sourcemaps briefly

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ const dirname =
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
 	plugins: [enhancedImages(), sveltekit()],
+	build: {
+		sourcemap: 'inline'
+	},
 	test: {
 		projects: [
 			{
