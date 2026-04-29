c00aa518d761744226d8300cf2c2efcedba75e7f
Rookie Nguyen <nhqthang@proton.me>
2026-04-29 15:32:29 +0700

    chore: ditching source maps completely

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,6 @@ const dirname =
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
 	plugins: [enhancedImages(), sveltekit()],
-	build: {
-		sourcemap: 'inline'
-	},
 	test: {
 		projects: [
 			{
