a6bfbf2ae63bf80d775523708bbe2e5c56800830
Rookie Nguyen <nhqthang@proton.me>
2026-05-04 13:21:23 +0700

    refactor: renaming the used `staticPages` to a list of links to display to `mainMenu`

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -13,10 +13,11 @@ const siteConfig = {
 		{ name: 'Email', url: 'mailto:nhqthang@itsrookienguyen.me', icon: Mail },
 		{ name: 'GitHub', url: 'https://github.com/rookie001mc', icon: SiGithub }
 	],
-	staticPages: [
+	mainMenu: [
 		{ label: 'Home', path: '/' },
 		{ label: 'About', path: '/about' },
-		{ label: 'Blog', path: '/blog' }
+		{ label: 'Blog', path: '/blog' },
+		{ label: 'Contact', path: '/contact' }
 	],
 	leftFooterMenu: [
 		{ label: 'Home', path: '/' },
