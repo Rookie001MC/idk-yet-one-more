befe9a3f35c0b8167aabff3f6688ab92b94578ce
Rookie Nguyen <nhqthang@proton.me>
2026-05-04 15:04:29 +0700

    chore: removing `@sveltejs/enhanced-img`

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest/config" />
 import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -11,7 +10,7 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()],
+	plugins: [sveltekit()],
 	test: {
 		projects: [
 			{
