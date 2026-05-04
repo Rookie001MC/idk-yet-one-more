7f0257210733f4ff731524e9fd69b19752bfef63
Rookie Nguyen <nhqthang@proton.me>
2026-05-04 18:55:08 +0700

    feat: finally finishing `sitemap.xml`

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,9 +1,25 @@
-import * as sitemap from 'super-sitemap';
-import { PUBLIC_BASE_URL } from '$env/static/public';
 import type { RequestHandler } from '@sveltejs/kit';
+import * as sitemap from 'super-sitemap';
+import { fetchAllPosts } from '$lib/data/blogPosts';
+import { fetchCategories } from '$lib/data/categories';
+import { fetchTags } from '$lib/data/tags';
+import { PUBLIC_BASE_URL } from '$env/static/public';
 
-export const GET: RequestHandler = async () => {
+export const prerender = true;
+
+export const GET: RequestHandler = async ({ fetch }) => {
+	const [posts, categories, tags] = await Promise.all([
+		fetchAllPosts(['slug'], fetch),
+		fetchCategories(fetch),
+		fetchTags(fetch)
+	]);
+
 	return await sitemap.response({
-		origin: PUBLIC_BASE_URL
+		origin: PUBLIC_BASE_URL,
+		paramValues: {
+			'/blog/[slug]': posts.map((p) => p.slug),
+			'/category/[slug]': categories.map((c) => c.slug),
+			'/tag/[slug]': tags.map((t) => t.slug)
+		}
 	});
 };
