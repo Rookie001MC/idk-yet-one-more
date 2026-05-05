c6f55fbb57f52f330f101f95a696908863a4fb1a
Rookie Nguyen <nhqthang@proton.me>
2026-05-05 23:55:39 +0700

    chore: added some security headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,9 +3,16 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	return await resolve(event, {
+	const response = await resolve(event, {
 		filterSerializedResponseHeaders: (key, value) => {
 			return key.toLowerCase() === 'content-type';
 		}
 	});
+
+	response.headers.set('Permission-Policy', 'interest-cohort=()');
+	response.headers.set('X-Content-Type-Options', 'nosniff');
+	response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+	response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+
+	return response;
 };
