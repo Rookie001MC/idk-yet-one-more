d148ceccf00cde5f86b8a61475d3c5e739cdb648
Rookie Nguyen <nhqthang@proton.me>
2026-05-06 01:33:52 +0700

    Of course it's CSP

--- svelte.config.js
+++ svelte.config.js
@@ -3,41 +3,45 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-				preprocess: [vitePreprocess()],
-				compilerOptions: {
-								// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-								runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-				},
-				kit: {
-				 adapter: adapter(),
+	preprocess: [vitePreprocess()],
+	compilerOptions: {
+		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+	},
+	kit: {
+		adapter: adapter(),
 
-				 csp: {
-									mode: 'hash',
-									directives: {
-													'default-src': ["'none'"],
-													'script-src': ["'self'"],
-													'style-src': ["'self'", "'unsafe-inline'"],
-													'img-src': ["'self'", 'https:', 'data:', 'blob:'], // Consider that I might embed some other stuff in here
-													'connect-src': ["'self'", `${process.env.PUBLIC_DIRECTUS_URL}`],
-													'form-action': ["'self'"],
-													'frame-src': ["'self'", 'https:'],
-													'frame-ancestors': ["'self'", `${process.env.PUBLIC_DIRECTUS_URL}`],
-													'font-src': ["'self'"],
-													'object-src': ["'none'"],
-													'base-uri': ["'self'"]
-									}
-					},
+		csp: {
+			mode: 'hash',
+			directives: {
+				'default-src': ["'none'"],
+				'script-src': ["'self'"],
+				'style-src': ["'self'", "'unsafe-inline'"],
+				'img-src': ["'self'", 'https:', 'data:', 'blob:'], // Consider that I might embed some other stuff in here
+				'connect-src': [
+					"'self'",
+					`${process.env.PUBLIC_DIRECTUS_URL}`,
+					'https://o4508445764550656.ingest.de.sentry.io'
+				],
+				'form-action': ["'self'"],
+				'frame-src': ["'self'", 'https:'],
+				'frame-ancestors': ["'self'", `${process.env.PUBLIC_DIRECTUS_URL}`],
+				'font-src': ["'self'"],
+				'object-src': ["'none'"],
+				'base-uri': ["'self'"]
+			}
+		},
 
-				 experimental: {
-					 tracing: {
-						 server: true
-						},
+		experimental: {
+			tracing: {
+				server: true
+			},
 
-					 instrumentation: {
-						 server: true
-						}
-					}
-				}
+			instrumentation: {
+				server: true
+			}
+		}
+	}
 };
 
 export default config;
